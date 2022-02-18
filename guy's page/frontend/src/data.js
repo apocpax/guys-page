18a6a3f0100da5343d1129cb7f86acdb45f6e2ea
apocpax <richard.c.lummus90@gmail.com>
2022-02-17 20:42:08 -0800

    first products

---
+++ guy's page/frontend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products: [
+        {
+            name: 'guys rainbow',
+            slug: 'guys-rainbow',
+            image: '/images/guys-rainbow.jpg',
+            price: 14.99,
+            countInStock: 10,
+            brand: 'guy',
+            rating: 5.0,
+            numReviews: 10,
+            description: 'sticker',
+        },
+          {
+            name: 'guys rainbow',
+            slug: 'guys-rainbow',
+            image: '/images/guys-rainbow.jpg',
+            price: 14.99,
+            countInStock: 10,
+            brand: 'guy',
+            rating: 5.0,
+            numReviews: 10,
+            description: 'sticker',
+        },
+          {
+            name: 'guys rainbow',
+            slug: 'guys-rainbow',
+            image: '/images/guys-rainbow.jpg',
+            price: 14.99,
+            countInStock: 10,
+            brand: 'guy',
+            rating: 5.0,
+            numReviews: 10,
+            description: 'sticker',
+        },
+    ]
+};
+export default data;
